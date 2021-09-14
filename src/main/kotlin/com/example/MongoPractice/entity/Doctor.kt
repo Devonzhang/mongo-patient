e988695f7e225f360f6fbee27d184fa88e25c4a7
Devonzhang <devonzhang1024@gmail.com>
2021-09-14 18:10:18 +0800

    refactor: change entity id to uuid

--- src/main/kotlin/com/example/MongoPractice/entity/Doctor.kt
+++ src/main/kotlin/com/example/MongoPractice/entity/Doctor.kt
@@ -1,9 +1,11 @@
 package com.example.MongoPractice.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import java.util.*
 
 @Document
 data class Doctor(
@@ -13,5 +15,5 @@ data class Doctor(
     val createdDate: LocalDateTime = LocalDateTime.now(),
     var modifiedDate: LocalDateTime = LocalDateTime.now(),
     @Id
-    val id: ObjectId = ObjectId.get()
+    val id: UUID = UUID.randomUUID()
 )
--- src/main/kotlin/com/example/MongoPractice/entity/Patient.kt
+++ src/main/kotlin/com/example/MongoPractice/entity/Patient.kt
@@ -1,19 +1,18 @@
 package com.example.MongoPractice.entity
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import org.bson.types.ObjectId
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import java.util.*
 
 @Document
 data class Patient(
     val name: String,
     val description: String,
-    val doctor: Doctor,
+    var doctor: Doctor,
     val createdDate: LocalDateTime = LocalDateTime.now(),
-    val modifiedDate: LocalDateTime = LocalDateTime.now(),
+    var modifiedDate: LocalDateTime = LocalDateTime.now(),
     @Id
-    @JsonSerialize
-    val id: ObjectId = ObjectId.get()
+    val id: UUID = UUID.randomUUID()
 )
