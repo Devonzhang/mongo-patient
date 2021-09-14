21c01056d60c4893f8132b3ec7b32583735ad5c7
Devonzhang <devonzhang1024@gmail.com>
2021-09-14 18:13:59 +0800

    fix: fix the bug that the number of patients didn't change when that doctor has a new patient

--- src/main/kotlin/com/example/MongoPractice/service/PatientAddDoctorService.kt
+++ src/main/kotlin/com/example/MongoPractice/service/PatientAddDoctorService.kt
@@ -4,23 +4,35 @@ import com.example.MongoPractice.dto.PatientRequest
 import com.example.MongoPractice.entity.Doctor
 import com.example.MongoPractice.entity.Patient
 import com.example.MongoPractice.repository.DoctorRepository
+import com.example.MongoPractice.repository.PatientRepository
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
 class PatientAddDoctorService(
     private val doctorRepository: DoctorRepository,
+    private val patientRepository: PatientRepository
 ) {
     fun addDoctor(patient: PatientRequest): Patient {
         val doctor = selectDoctor()
+        updatePatients(doctor)
         return Patient(patient.name, patient.description, doctor)
     }
 
     fun selectDoctor(): Doctor {
-        var doctor = doctorRepository.findAll().minByOrNull { it.patientNumber }!!
+        val doctor = doctorRepository.findAll().minByOrNull { it.patientNumber }!!
         doctor.patientNumber++
         doctor.modifiedDate = LocalDateTime.now()
         doctorRepository.save(doctor)
         return doctor
+    }
+
+    fun updatePatients(doctor: Doctor) {
+        val patientsNeedUpdate = patientRepository.findAll().filter { it.doctor.id == doctor.id }
+        patientsNeedUpdate.forEach {
+            it.doctor = doctor
+            it.modifiedDate = LocalDateTime.now()
+        }
+        patientRepository.saveAll(patientsNeedUpdate)
     }
 }
